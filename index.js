82dc975dfd8b81a186c5fde7f5861f9c4aae2969
Donray Williams <donrayxwilliams@gmail.com>
2022-11-04 12:44:33 -0500

    update users endpoint

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ app.get('/', function (request, response) {
 app.get('/users', function (req, res) {
   // Hard coding for simplicity. Pretend this hits a real database
   res.json([
-    { "id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com" },
-    { "id": 2, "firstName": "Tammy", "lastName": "Norton", "email": "tnorton@yahoo.com" },
-    { "id": 3, "firstName": "Tina", "lastName": "Lee", "email": "lee.tina@hotmail.com" }
+    { 'id': 1, 'name': 'Don' },
+    { 'id': 2, 'name': 'Chay' },
+    { 'id': 3, 'name': 'Dev' }
   ]);
 });
 
