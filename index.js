6a4e57289c339115a310e057c5a0315d8af48553
Donray Williams <donrayxwilliams@gmail.com>
2022-11-04 10:56:38 -0500

    Make app my own

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express =  require('express');
+var express = require('express');
 var cors = require('cors');
 
 const app = express();
@@ -6,19 +6,19 @@ app.use(cors());
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.send('Hello World!')
 });
 
-app.get('/users', function(req, res) {
+app.get('/users', function (req, res) {
   // Hard coding for simplicity. Pretend this hits a real database
   res.json([
-    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
-    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
-    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+    { "id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com" },
+    { "id": 2, "firstName": "Tammy", "lastName": "Norton", "email": "tnorton@yahoo.com" },
+    { "id": 3, "firstName": "Tina", "lastName": "Lee", "email": "lee.tina@hotmail.com" }
   ]);
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port')); 
+app.listen(app.get('port'), function () {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
